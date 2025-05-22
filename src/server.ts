13d58477e1861553226ad3ba042019081da16210
AlaMdalla <101179732+AlaMdalla@users.noreply.github.com>
2025-05-22 16:22:59 +0100

    old orders

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin:[ 'https://marimex.netlify.app','https://marimex-frontt-6ti8.vercel.app/'],
+  origin:'https://marimex.netlify.app',
  credentials: true,
 };
 
